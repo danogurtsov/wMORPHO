7c7a20d5945c4622bc8242df129ce4cde33332cd
danogurtsov <dan.ogurtsov@gmail.com>
2024-06-06 19:11:38 +0300

    renaming

--- test/WMORPHO.sol
+++ test/WMORPHO.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {WMORPHO} from "../src/WMORPHO.sol";
 
-contract CounterTest is Test {
+contract WMORPHOTest is Test {
     WMORPHO public wMorpho;
     address morpho = 0x9994E35Db50125E0DF82e4c2dde62496CE330999;
 
